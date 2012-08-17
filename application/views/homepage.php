8a4fc4b66e6651060edc3d99cc3fd50a579c4fa1
Dan Mesa <dan@danmesa.com>
2012-08-17 19:27:13 -0400

    Sign up form UI

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -10,3 +10,35 @@
 
 
 <h1>Homepage</h1>
+
+<hr />
+
+<h2>Ready to Sign Up?</h2>
+<form class="form-horizontal">
+  <fieldset>
+    <legend>Just a few quick details:</legend>
+    <div class="control-group">
+      <label class="control-label" for="input01">Email</label>
+      <div class="controls">
+        <input type="text" class="input-xlarge" id="input01">
+        <p class="help-block">Supporting help text</p>
+      </div>
+    </div>
+    <div class="control-group">
+      <label class="control-label" for="input01">Password</label>
+      <div class="controls">
+        <input type="text" class="input-xlarge" id="input01">
+      </div>
+    </div>
+    <div class="control-group">
+      <label class="control-label" for="input01">Confirm Password</label>
+      <div class="controls">
+        <input type="text" class="input-xlarge" id="input01">
+        <p class="help-block">Let's see if we can figure out a way to not have to do this.</p>
+      </div>
+    </div>
+    <div class="form-actions">
+		<button type="submit" class="btn btn-primary">Sign Up!</button>
+	</div>
+  </fieldset>
+</form>
